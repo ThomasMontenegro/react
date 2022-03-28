6db6bf086f6c35bcc12468f1f7ba90d449a30db4
thomas montenegro <montenegrothomasagustin@gmail.com>
2022-03-28 18:33:45 -0300

    navbar

--- bambina/bambina/src/App.js
+++ bambina/bambina/src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
 
 
 export default function App() {
   return (
     <>
+  <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Bambina Disegno</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Inicio</Nav.Link>
+      <NavDropdown title="Productos" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Mates</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Velas</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Almohadones</NavDropdown.Item>
+      </NavDropdown>
+      <Nav.Link href="#pricing">Nosotros</Nav.Link>
+      <Nav.Link href="#pricing">Ayuda</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
     </>
   );
 }

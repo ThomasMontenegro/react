498b244b59dcde06a3af1b0b6bea2a9191683087
thomas montenegro <montenegrothomasagustin@gmail.com>
2022-03-28 19:27:21 -0300

    font

--- bambina/bambina/src/App.js
+++ bambina/bambina/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
 
@@ -6,10 +7,10 @@ import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
 export default function App() {
   return (
     <>
-  <Navbar bg="dark" variant="dark">
-    <Container>
+  <Navbar variant="dark" style={{ backgroundColor: "#EC407A" }}>
+    <Container style={{ backgroundColor: "#EC407A" }}>
     <Navbar.Brand href="#home">Bambina Disegno</Navbar.Brand>
-    <Nav className="me-auto">
+    <Nav className="m-right">
       <Nav.Link href="#home">Inicio</Nav.Link>
       <NavDropdown title="Productos" id="basic-nav-dropdown">
           <NavDropdown.Item href="#action/3.1">Mates</NavDropdown.Item>

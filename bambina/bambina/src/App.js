5f1d08c9465eb89b27d8950aff0a7b2e778d6b35
thomas montenegro <montenegrothomasagustin@gmail.com>
2022-03-31 13:59:58 -0300

    widget y itemList

--- bambina/bambina/src/App.js
+++ bambina/bambina/src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBarr from "./components/NavBarr";
+import ItemListContainer from "./components/ItemListContainer";
+
 
 
 export default function App() {
   return (
     <>
     <NavBarr />
+    <ItemListContainer/>
     </>
   );
 }
---
+++ bambina/bambina/src/components/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Greetings from "./Greetings";
+
+export default function ItemListContainer() {
+    return(
+        <>
+        <Greetings name="nuevo usuario"/>
+        </>
+    );
+}

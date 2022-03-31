c784d699ce871ace686fc6d25b981dee8a251525
thomas montenegro <montenegrothomasagustin@gmail.com>
2022-03-31 13:24:32 -0300

    icono

---
+++ bambina/bambina/src/components/CartWidget.jsx
@@ -0,0 +1,10 @@
+import * as  React from "react";
+import '../App.css';
+import {AiOutlineShoppingCart } from "react-icons/ai";
+export default function CartWidget() {
+    return(
+        <>
+        <span className="icono"><AiOutlineShoppingCart/></span>
+        </>
+    );
+}
--- bambina/bambina/src/components/NavBarr.jsx
+++ bambina/bambina/src/components/NavBarr.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
+import CartWidget from "./CartWidget";
+import logo from "./logo-bambina.png";
 
 
-export default function NavBar() {
+
+export default function NavBarr() {
   return (
     <>
-  <Navbar bg="dark" variant="dark">
-    <Container>
+  <Navbar variant="dark" style={{ backgroundColor: "#ff3e99" }}>
+    <Container fluid>
+         <img src={logo} className="logo"/>
     <Navbar.Brand href="#home">Bambina Disegno</Navbar.Brand>
-    <Nav className="me-auto">
+    <Nav className="m-right">
       <Nav.Link href="#home">Inicio</Nav.Link>
       <NavDropdown title="Productos" id="basic-nav-dropdown">
           <NavDropdown.Item href="#action/3.1">Mates</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.2">Velas</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.3">Almohadones</NavDropdown.Item>
       </NavDropdown>
-      <Nav.Link href="#pricing">Nosotros</Nav.Link>
-      <Nav.Link href="#pricing">Ayuda</Nav.Link>
+      <Nav.Link>Nosotros</Nav.Link>
+      <Nav.Link>Carrito</Nav.Link>
+      <CartWidget/>
     </Nav>
     </Container>
   </Navbar>

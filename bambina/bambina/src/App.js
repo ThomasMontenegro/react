6d53c41ded842af35295d7234f8c8864b5e2423d
thomas montenegro <montenegrothomasagustin@gmail.com>
2022-04-07 20:14:49 -0300

    creacion routes

--- bambina/bambina/src/App.js
+++ bambina/bambina/src/App.js
@@ -3,16 +3,19 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBarr from "./components/NavBarr";
 import ItemListContainer from "./components/items/ItemListContainer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 
 export default function App() {
   return (
     <>
-    <NavBarr />
-    <div className="d-flex justify-content-center">
-      <ItemListContainer/>
-    </div>
+    <BrowserRouter>
+      <NavBarr />
+      <Routes>
+      <Route exact path="/" element={<ItemListContainer/>}/>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }

f235bc171119969df6c71616de3fc92045b7d692
thomas montenegro <montenegrothomasagustin@gmail.com>
2022-04-24 22:52:21 -0300

    iniciando firebase

--- bambina/bambina/src/index.js
+++ bambina/bambina/src/index.js
@@ -4,6 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWYeXbaOS9zw4fL5UO4h6rzBDR5Hd3J8Y",
+  authDomain: "bambina-disegno.firebaseapp.com",
+  projectId: "bambina-disegno",
+  storageBucket: "bambina-disegno.appspot.com",
+  messagingSenderId: "567240577259",
+  appId: "1:567240577259:web:5fa7c679072410c48d2eae"
+};
+
+initializeApp(firebaseConfig);
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />

1ab8a5248ae62b1241f94def512a0d7615e31e17
thomas montenegro <montenegrothomasagustin@gmail.com>
2022-05-02 20:10:22 -0300

    terminando firebase 2

--- bambina/bambina/src/components/Checkout.jsx
+++ bambina/bambina/src/components/Checkout.jsx
@@ -1,85 +1,59 @@
-import React from 'react';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button"
+import Form from "react-bootstrap/Form"
+import Container from "react-bootstrap/Container"
 import { useContext } from 'react';
 import { CartContext} from './CartContext';
-import { doc, addDoc, collection, getDoc, getFirestore, Timestamp, updateDoc } from 'firebase/firestore';
-import { useForm } from 'react-hook-form';
-import { useState } from 'react';
-import { Link, Navigate } from 'react-router-dom';
+import {collection,addDoc, getFirestore} from "firebase/firestore"
+export  default function Cart() {
 
-export default function Checkout() {
-    const {cart, totalCart, cleanCart, totalItems} = useContext(CartContext)
-    const { register, handleSubmit } = useForm();
+    const {cart,totalCart} = useContext(CartContext)
 
-    const [orderId, setOrderId] = useState()
+    const [name, setName] =   useState();
+    const [email, setEmail] = useState();
+    const [phone, setPhone] = useState();
 
-    const onSubmit = data => {
-        const sendOrder = () => {
-            const order = {
-                buyer:{
-                    name: data.Name,
-                    email:data.Email,
-                    tel:data.Tel,
-                    date: Timestamp.fromDate(new Date()),
-                },
-                items: cart,
-                total: totalCart()
-            }
-            
-            const db = getFirestore();
-            const ordersCollection = collection(db, 'orders')
-            addDoc(ordersCollection, order)
-            .then((res)=> {
-                cart.forEach((item)=>{
-                    const docRef = doc(db, 'productos', item.id)
-                    getDoc(docRef)
-                        .then((dbDoc)=>{
-                            updateDoc(docRef, {stock:dbDoc.data().stock-item.number})
-                        })
-                })
-                setOrderId(res.id)
-                cleanCart()
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-        }
-        sendOrder()
-        }
-        
-        if (orderId) {
-            return (
-                <>
-                <div>
-                    <h2>Gracias por tu compra!</h2>
-                    <h3>Tu numero de orden es: </h3>
-                    <h3><b>{orderId}</b></h3>
-                    <Link to='/'><button className='boton-volver'>Volver</button></Link>
-                </div>
-                </>
-            )
-        }
-        
-        if(cart.length === 0){
-            alert('No hay items en el carrito')
-            return <Navigate to='/'/>
-        }
+   function  terminarCompra () {
+
+    const db = getFirestore();
+    const Orders =collection(db,"orders");
     
-    return (
+
+    let buyer = {
+        buyer: { name, phone, email },
+        items: cart,
+        total: totalCart(),
+    }; 
+
+    addDoc(Orders,buyer).then(({id}) => {
+        console.log(id)
+    })
+
+    alert("Tu pedido se ha registrado" + "\n"+ name +"\n"+ email +"\n" +phone);
+    }
+    
+    return(
         <>
-            <center>
-            <form onSubmit={handleSubmit(onSubmit)} className='checkout'>
-                <div className='resumen-compra'>
-                    <h3 className='legend'>Resumen de tu compra:</h3>
-                    <p className='legend1'>{totalItems()} productos</p>
-                    <p className='legend1'>Total a pagar: ${totalCart()}</p>
-                </div>
-                <legend className='legend'>Ingrese sus datos para finalizar:</legend>
-                <input type="text" className="formulario-input" placeholder="Nombre Completo" {...register("Name", {required: true, maxLength: 80})} />
-                <input type="text" className="formulario-input" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
-                <input type="tel" className="formulario-input" placeholder="Mobile number" {...register("Tel", {required: true, minLength: 6, maxLength: 12})} />
-                <input type ="submit" className="formulario-submit"/>
-            </form>
-            </center>       
-    </>
-    );
+      <Container className="justify-content-center">
+
+        <p>Pone tus datos para finalizar la compra</p>
+        <Form>
+            
+            <input placeholder={"Nombre y apellido"}  type={"text"} value={name}
+            onChange={(e) => {setName(e.currentTarget.value);}}/>
+
+            <input placeholder={"E-mail"} type={"email"} value={email}
+            onChange={(e) => {setEmail(e.currentTarget.value);}}/>
+
+            <input placeholder={"Numero de Celular"} type={"number"} value={phone}
+            onChange={(e) => {setPhone(e.currentTarget.value);}}/>
+        </Form>
+      </Container>
+                    <Form className="d-flex">
+                        <Button variant="outline-success" onClick={terminarCompra}>Finalizar Comprar</Button>
+                    </Form>
+        </>
+    )
+   
+   
 }
